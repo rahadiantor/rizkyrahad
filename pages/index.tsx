14b61a7deacfb241f3ff3d94a3c7026a40abd4d4
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-07-16 19:36:53 +0700

    setup github forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import Head from "next/head";
 import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
 import { GetStaticProps } from "next";
 
+import { usePlugin } from "tinacms";
+import { useGithubJsonForm } from "react-tinacms-github";
+
 export default function Home({ file }) {
-  const data = file.data;
+  const formOptions = {
+    label: "Home Page",
+    fields: [{ name: "title", component: "text" }],
+  };
+
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
   return (
     <div className="container">
       <Head>
