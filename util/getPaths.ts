01d4ce8f781f33d4375e92c0e409db8855e92b70
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-07-18 20:19:52 +0700

    log all kinds of path

--- util/getPaths.ts
+++ util/getPaths.ts
@@ -1,8 +1,19 @@
 import { join, resolve } from "path";
 
 // `public/narrative/`
-export const getPublicPath = (...dirs: string[]) =>
-  resolve(join("public", ...dirs));
+export const getPublicPath = (...dirs: string[]) => {
+  const resolvedPath = resolve(join("public", ...dirs));
+  const processCwdPath = join(process.cwd(), "public", ...dirs);
+  const resolvedStaticPath = resolve(`../public/${dirs.join("/")}`);
+  const dirnamePath = join(__dirname, "public", ...dirs);
+  console.log({
+    resolvedPath,
+    resolvedStaticPath,
+    processCwdPath,
+    dirnamePath,
+  });
+  return resolvedPath;
+};
 
 export const getContentPath = (...dirs: string[]) =>
   resolve(join("content", ...dirs));
