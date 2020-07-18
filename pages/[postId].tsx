806187c48786876a71872190572e1988b91f3465
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2020-07-18 16:58:46 +0700

    add barebones for narratives type

---
+++ pages/[postId].tsx
@@ -0,0 +1,74 @@
+import fs from "fs";
+import { join } from "path";
+
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import { usePlugin } from "tinacms";
+import { useGithubJsonForm } from "react-tinacms-github";
+
+export default function Narrative({ file }) {
+  const formOptions = {
+    label: "Narrative",
+    fields: [
+      { name: "title", component: "text" },
+      { name: "slug", component: "text" },
+      { name: "description", component: "text" },
+      { name: "role", component: "text" },
+    ],
+  };
+
+  const [data, form] = useGithubJsonForm(file, formOptions);
+  usePlugin(form);
+
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <b>{data.role}</b>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+  params,
+}) {
+  const path = `content/narrative/${params.postId}.json`;
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: path,
+      parse: parseJson,
+    });
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: path,
+        data: (await import(`../${path}`)).default,
+      },
+    },
+  };
+};
+
+export const getStaticPaths = async function () {
+  const narratives = fs.readdirSync(
+    join(process.cwd(), "content", "narrative")
+  );
+  return {
+    paths: narratives.map((fileName) => ({
+      params: {
+        postId: fileName.split(".")[0],
+      },
+    })),
+    fallback: false,
+  };
+};
